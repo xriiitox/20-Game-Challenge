0da24fa4aad1db3151551665e52643266b608755
Josh Baron <50253167+xriiitox@users.noreply.github.com>
2024-08-02 23:48:22 -0400

    fix speed

--- Ponge/TheOpponent.gd
+++ Ponge/TheOpponent.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-var speed := 4.2
+var speed := 150
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,8 +9,15 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	var velocity := Vector2.ZERO
 	if position.y < $"../Ball".position.y:
-		position.y += speed
+		velocity.y += speed
 	elif position.y > $"../Ball".position.y:
-		position.y -= speed
+		velocity.y -= speed
+		
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, get_viewport_rect().size)
 	
