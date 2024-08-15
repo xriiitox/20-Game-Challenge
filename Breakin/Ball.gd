373f453b191bef2ae44ff9b658c1036ac2499c11
Josh Baron <50253167+xriiitox@users.noreply.github.com>
2024-08-15 15:36:32 -0400

    full game!

--- Breakin/Ball.gd
+++ Breakin/Ball.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
 @export var speed: float
+var lives := 3
+@onready var Main := $".."
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,26 +13,40 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
+	if Main.bricks_left == 0:
+		$"../WinText".visible = true
+		$"../Win".play()
+		process_mode = Node.PROCESS_MODE_DISABLED
 	var collision = move_and_collide(velocity.normalized() * speed * delta)
-	
-	if global_position.x >= 1280 or global_position.x <= 0:
-		velocity.x = -velocity.x
 	
 	if global_position.y <= 0:
 		velocity.y = -velocity.y
+		$"../PaddleSFX".play()
 	
 	if global_position.y >= 720:
 		process_mode = Node.PROCESS_MODE_DISABLED
 		global_position = Vector2(240, 360)
 		velocity = Vector2(1, 1).normalized()
+		get_node("../Lives/" + str(lives)).queue_free()
+		lives -= 1
+		$"../Lose".play()
+		if lives == 0:
+			get_tree().change_scene_to_file("res://lose.tscn")
 	
 	if collision:
-		if collision.get_collider().has_method("hit"):
+		var collider := collision.get_collider()
+		if collider.has_method("hit"):
 			velocity = velocity.bounce(collision.get_normal())
 			collision.get_collider().hit()
+			Main.bricks_left -= 1
 			$"../Score".text = str(int($"../Score".text) + 10)
+			$"../Brick".play()
+		elif collider.name == "WallLeft" or collider.name == "WallRight":
+			velocity = velocity.bounce(collision.get_normal())
+			$"../PaddleSFX".play()
 		else:
 			velocity = global_position - collision.get_collider().global_position
+			$"../PaddleSFX".play()
 		velocity = velocity.normalized() * speed * delta
 
 func _on_paddle_balls():
--- Breakin/main.gd
+++ Breakin/main.gd
@@ -1,14 +1,17 @@
-extends Node2D
+class_name Main extends Node2D
 
 var brick = preload("res://brick.tscn")
+var bricks_left := 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for i in 8:
 		for j in 14:
 			var new_brick := brick.instantiate()
+			# wacky ass position calculation
 			new_brick.position = Vector2(45 + j * 90 + 10, 50 + i * 30 + 10)
 			add_child(new_brick)
+			bricks_left += 1
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
